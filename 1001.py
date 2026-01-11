0564784363af141df3d904f8ef376587d3a58693
Heorhii Ponomarenko <ge.ponomarenko@gmail.com>
2026-01-11 23:30:50 +0100

    Added error checking when entering data and fixed some bugs.

--- 1001.py
+++ 1001.py
@@ -33,70 +33,146 @@ cursor.execute("""
 
 
 while True:
-    print("----------------------------------------\n1.Додати нового студента.\n2.Додати новий курс. \n3.Показати список студентів.\n4.Показати список курсів\n5.Записати студента на курс\n6.Показати студентів на конкретному курсі\n7.Завершення роботи.\n----------------------------------------\n")
+    print("""\n---------------------------------------------------------------
+1.Додати нового студента.
+2.Додати новий курс.
+3.Показати список студентів.
+4.Показати список курсів.
+5.Записати студента на курс.
+6.Показати студентів на конкретному курсі.
+7.Завершення роботи.
+---------------------------------------------------------------\n""")
+    try:
+        choice = int(input("Виберіть опцію (1 - 7): \n"))
+    except ValueError:
+        print("\nВиникла помилка, виберіть опцію ще раз!\n")
     
-    choice = int(input("Виберіть опцію (1 - 7):\n"))
-    if choice == 7:
-        break
-
-    elif choice == 1:
-        print("Додавання нового студента.\n")
-        name = input("Ім'я нового студента:")
-        age = int(input("Вік студента:"))
-        major = input("Спеціальність студента:")
-        query = "INSERT INTO students (name, age,major)" \
-            "VALUES (?, ?, ?)"
-        cursor.execute(query, (name, age, major))
-        conn.commit()
-
-    elif choice == 2:
-        print("Додавання нового курса.\n")
-        name = input("Ім'я нового курса:")
-        instruktor = input("Ім'я викладача курса:")
-        query = "INSERT INTO courses (courses_name, instructor)" \
-            "VALUES (?, ?)"
-        cursor.execute(query, (name, instruktor))
-        conn.commit()
-
-    elif choice == 3:
-        print("\nСписок студентів.\n")
-        query = "SELECT * FROM students"
-        cursor.execute(query)
-        all_students = cursor.fetchall()
-        for student in all_students:
-            print(f"Нумер студента - {student[0]}")
-            print(f"Ім'я студента - {student[1]}")
-            print(f"Вік студента - {student[2]}")
-            print(f"Спеціальність студента - {student[3]}")
-
-    elif choice == 4:
-        print("\nСписок курсів.\n")
-        query = "SELECT * FROM courses"
-        cursor.execute(query)
-        all_courses = cursor.fetchall()
-        for course in all_courses:
-            print(f"Нумер курсу - {course[0]}")
-            print(f"Ім'я курсу - {course[1]}")
-            print(f"Ім'я викладача - {course[2]}")
+    else:
     
-    elif choice == 5:
-        student_id = int(input("Введіть номер студента:"))
-        course_id = int(input("Введіть номер курсу:"))
-        query = "INSERT INTO info (student_id, course_id)" \
-        "VALUES (?, ?)"
-        cursor.execute(query, (student_id, course_id))
-        conn.commit()
 
-    elif choice == 6:
-        print("\nПоказати студентів на конкретному курсі.\n")
-        course_id = int(input("Введіть номер курсу:"))
-        query = "SELECT * FROM students WHERE student_id = (" \
-        "SELECT student_id FROM info WHERE couse_id = (" \
-        "SELECT course_id FROM courses WHERE course_name = ?))"
-        cursor.execute(query, (course_id))
-        all_students = cursor.fetchall()
-        for student in all_students:
-            print(f"Нумер студента - {student[0]}")
-            print(f"Ім'я студента - {student[1]}")
-            print(f"Вік студента - {student[2]}")
-            print(f"Спеціальність студента - {student[3]}")
+        if choice == 7:
+            break
+
+
+        elif choice == 1:
+            print("Додавання нового студента.\n")
+            while True:
+                name = input("Ім'я нового студента: ")
+                if not name.isalpha():
+                    print("\nВиникла помилка, введіть ім'я студента правильно!\n") 
+                else:
+                    try:
+                        age = int(input("Вік студента: "))
+                    except ValueError:
+                        print("\nВиникла помилка, введіть вік студента правильно.\n")
+                    else:
+                        major = input("Спеціальність студента: ")
+                        if not major.isalpha():
+                            print("\nВиникла помилка, введіть спеціальність студента правильно.\n")
+                        else:
+                            break
+            query = "INSERT INTO students (name, age,major)" \
+                "VALUES (?, ?, ?)"
+            cursor.execute(query, (name, age, major))
+            conn.commit()
+
+
+        elif choice == 2:
+            print("Додавання нового курса.\n")
+            while True:
+                name = input("Ім'я нового курса: ")
+                if not name.isalpha():
+                    print("\nВиникла помилка, введіть назву курса правильно!\n")
+                else:
+                    instruktor = input("Ім'я викладача курса: ")
+                    if not instruktor.isalpha():
+                        print("\nВиникла помилка, введіть ім'я викладача правильно.\n")
+                    else:
+                        break
+
+            query = "INSERT INTO courses (courses_name, instructor)" \
+                "VALUES (?, ?)"
+            cursor.execute(query, (name, instruktor))
+            conn.commit()
+
+
+        elif choice == 3:
+            print("\nСписок студентів.\n")
+            query = "SELECT * FROM students"
+            cursor.execute(query)
+            all_students = cursor.fetchall()
+            for student in all_students:
+                print(f"Нумер студента - {student[0]}")
+                print(f"Ім'я студента - {student[1]}")
+                print(f"Вік студента - {student[2]}")
+                print(f"Спеціальність студента - {student[3]}\n")
+
+
+        elif choice == 4:
+            print("\nСписок курсів.\n")
+            query = "SELECT * FROM courses"
+            cursor.execute(query)
+            all_courses = cursor.fetchall()
+            for course in all_courses:
+                print(f"Нумер курсу - {course[0]}")
+                print(f"Ім'я курсу - {course[1]}")
+                print(f"Ім'я викладача - {course[2]}\n")
+
+
+        elif choice == 5:
+            while True:
+                try:
+                    student_id = int(input("Введіть номер студента: "))
+                except ValueError:
+                    print("\nВиникла помилка, введіть нумер студента правильно!\n")
+                else:
+                    query = "SELECT * FROM students WHERE id = ?"
+                    cursor.execute(query,(student_id,))
+                    students = cursor.fetchall()
+                    if students:
+                        try:
+                            course_id = int(input("Введіть номер курсу: "))
+                        except ValueError:
+                            print("\nВиникла помилка, введіть нумер курсу правльно!\n")
+                        else:
+                            query = "SELECT * FROM courses WHERE courses_id = ?"
+                            cursor.execute(query,(course_id,))
+                            all_courses = cursor.fetchall()
+                            if all_courses:
+                                query = "INSERT INTO info (students_id, course_id)" \
+                                "VALUES (?, ?)"
+                                cursor.execute(query, (student_id, course_id,))
+                                conn.commit()
+                                break
+
+
+        elif choice == 6:
+            while True:
+                try:
+                    course_id = int(input("Введіть номер курсу: "))
+                except ValueError:
+                    print("\nВиникла помилка, введіть нумер курсу правильно!\n")
+                else:
+                    query = "SELECT * FROM courses WHERE courses_id = ?"
+                    cursor.execute(query,(course_id,))
+                    all_courses = cursor.fetchall()
+                    if all_courses:
+                        
+                        query = """
+                        SELECT * 
+                        FROM students
+                        INNER JOIN info ON students.id = info.students_id 
+                        WHERE info.course_id = ?
+                        """
+
+                        cursor.execute(query, (course_id,))
+                        all_students = cursor.fetchall()
+                        print(all_students)
+                        print("\n-----------------------Список студентів-----------------------\n")
+                        for student in all_students:
+                            print(f"Нумер студента - {student[0]}")
+                            print(f"Ім'я студента - {student[1]}")
+                            print(f"Вік студента - {student[2]}")
+                            print(f"Спеціальність студента - {student[3]}\n")
+                        print("\n--------------------------------------------------------------\n")
+                        break
